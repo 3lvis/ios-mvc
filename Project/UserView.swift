f12ce2bde75867541a88690a7cb9e99e156f5329
Elvis Nuñez <elvisnunez@me.com>
2017-07-12 17:02:19 +0200

    First commit

---
+++ Project/UserView.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+class UserView: UIView {
+    
+}
---
+++ Project/UsersController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class UsersController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+}
