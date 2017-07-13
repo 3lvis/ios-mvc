77d6a76071189af099eafff099f2eb3b55f1f03f
Elvis Nuñez <elvisnunez@me.com>
2017-07-13 11:45:56 +0200

    Change users

--- Project/Model/User.swift
+++ Project/Model/User.swift
@@ -2,6 +2,14 @@ struct User {
     let name: String
 
     static var `default`: [User] {
-        return [User(name: "Elvis"), User(name: "Luna"), User(name: "Cocoa")]
+        return [
+            User(name: "Walter White"),
+            User(name: "Jesse Pinkman"),
+            User(name: "Saul Goodman"),
+            User(name: "Gus Fring"),
+            User(name: "Skyler White"),
+            User(name: "Hank Shrader"),
+            User(name: "Mike Ehrmantraut"),
+        ]
     }
 }
