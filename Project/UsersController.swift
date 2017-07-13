089843524faaf59e345e593906f3159fc7b1d04a
Elvis Nuñez <elvisnunez@me.com>
2017-07-13 11:06:11 +0200

    Add datasource + navigation bar

--- Project/UsersController.swift
+++ Project/UsersController.swift
@@ -10,4 +10,22 @@ class UsersController: UITableViewController {
 
         tableView.registerNib(UserCell.self)
     }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeue(UserCell.self, for: indexPath)
+    }
+
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let cell = cell as! UserCell
+        let name = dataSource[indexPath.row].name
+        cell.nameLabel?.text = name
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
